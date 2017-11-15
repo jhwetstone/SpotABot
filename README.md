b2a1fdca445c1dde65e4770609ea1d7d5c826e08
Jessica Wetstone <jess@superhuman.com>
2017-11-14 19:00:27 -0800

    adding load data utility, updating readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SpotABot
 Code for CS 229 Project I Spot a Bot
+
+## Getting Started
+1. Download cresci-2017.csv.zip
+1. Unzip each of the files into the SpotABot directory, in a folder labeled 'cresci-2017'
+1. Download cresci-2015.csv.zip
+1. Unzip each of the files into the SpotABot directory, in a folder labeled 'cresci-2015'
+1. Run load_data.py to load data into "genuine_tweets", "genuine_users", "bot_tweets", "bot_users"
+
+cresci-2015
+* Bot: INT, FSF, TWT
+* Not: TFP, E13
+
+cresci-2017
+* Bot: social_spambots_1, social_spambots_2, social_spambots_3, traditional_spambots_1, traditional_spambots_2, traditional_spambots_3, traditional_spambots_4
+* Not: genuine_accounts
---
+++ load_data.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 18:54:48 2017
+
+@author: jessicawetstone
+"""
+
+## Initialization
+import numpy as np
+import pandas as pd
+
+## Import datasets
+genuine_account_folders = ['cresci-2017/genuine_accounts.csv', 'cresci-2015/TFP.csv', 'cresci-2015/E13.csv']
+bot_account_folders = [
+                         'cresci-2017/social_spambots_1.csv'
+                        , 'cresci-2017/social_spambots_2.csv'
+                        , 'cresci-2017/social_spambots_3.csv'
+                        , 'cresci-2017/traditional_spambots_1.csv'
+                        , 'cresci-2015/INT.csv'
+                        , 'cresci-2015/FSF.csv'
+                        , 'cresci-2015/TWT.csv']
+
+list_genuine_users = []
+list_genuine_tweets = []
+for folder in genuine_account_folders:
+    df_users = pd.read_csv(folder+'/users.csv',index_col='id');
+    list_genuine_users.append(df_users);
+    df_tweets = pd.read_csv(folder+'/tweets.csv',index_col='id');
+    list_genuine_tweets.append(df_tweets);
+genuine_tweets = pd.concat(list_genuine_tweets)
+genuine_users = pd.concat(list_genuine_users)
+
+list_bot_users = []
+list_bot_tweets = []
+for folder in bot_account_folders:
+    df_users = pd.read_csv(folder+'/users.csv',index_col='id');
+    list_bot_users.append(df_users);
+    df_tweets = pd.read_csv(folder+'/tweets.csv',index_col='id');
+    list_bot_tweets.append(df_tweets);
+bot_tweets = pd.concat(list_bot_tweets)
+bot_users = pd.concat(list_bot_users)
+
+## Delete empty values
+del bot_users['contributors_enabled'];
+del bot_users['follow_request_sent'];
+del bot_users['following'];
+del bot_users['notifications'];
+del bot_users['test_set_1'];
+del bot_users['test_set_2'];
+
+del genuine_users['contributors_enabled'];
+del genuine_users['follow_request_sent'];
+del genuine_users['following'];
+del genuine_users['notifications'];
+del genuine_users['test_set_1'];
+del genuine_users['test_set_2'];
