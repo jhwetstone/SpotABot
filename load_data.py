7c2208205745c8d550c9e4cfaf5c0f7bab27f934
Jessica Wetstone <jess@superhuman.com>
2017-11-15 16:29:02 -0800

    stacking the table sooner and killing the duplicate code'

--- load_data.py
+++ load_data.py
@@ -46,76 +46,53 @@ for folder in bot_account_folders:
 bot_tweets = pd.concat(list_bot_tweets)
 bot_users = pd.concat(list_bot_users)
 
+users = pd.concat([bot_users,genuine_users])
+tweets = pd.concat([bot_tweets,genuine_tweets])
+user_class = pd.concat([pd.DataFrame(np.ones(shape=(len(bot_users),1))),pd.DataFrame(np.zeros(shape=(len(genuine_users),1)))])
+user_class.rename(columns={0:'is_bot'},inplace=True);
+
+## Delete empty values
+del users['contributors_enabled'];
+del users['follow_request_sent'];
+del users['following'];
+del users['notifications'];
+del users['test_set_1'];
+del users['test_set_2'];
+
+del tweets['geo'];
+del tweets['contributors'];
+del tweets['favorited'];
+del tweets['retweeted'];
+
+
 ## Add friend-follower ratio as a feature
-bot_users['friend_follower_ratio'] = bot_users['friends_count']/bot_users['followers_count']
+users['friend_follower_ratio'] = users['friends_count']/users['followers_count']
 ## Remove divide-by-0 errors
-bot_users.loc[~np.isfinite(bot_users['friend_follower_ratio']), 'friend_follower_ratio'] = np.nan
-genuine_users['friend_follower_ratio'] = genuine_users['friends_count']/genuine_users['followers_count']
-genuine_users.loc[~np.isfinite(genuine_users['friend_follower_ratio']), 'friend_follower_ratio'] = np.nan
+users.loc[~np.isfinite(users['friend_follower_ratio']), 'friend_follower_ratio'] = np.nan
 
 ## Add average-per-tweet information
-genuine_averages = genuine_tweets[['num_hashtags','reply_count','retweet_count','favorite_count','num_mentions','num_urls','user_id']].groupby('user_id').mean()
+averages = tweets[['num_hashtags','reply_count','retweet_count','favorite_count','num_mentions','num_urls','user_id']].groupby('user_id').mean()
 ## Reply count is sometimes blank -- replace with 0's
-genuine_averages['reply_count'] = genuine_averages['reply_count'].fillna(0)
-bot_averages = bot_tweets[['num_hashtags','reply_count','retweet_count','favorite_count','num_mentions','num_urls','user_id']].groupby('user_id').mean()
-bot_users = bot_users.join(bot_averages.add_suffix('_per_tweet'))
-genuine_users = genuine_users.join(genuine_averages.add_suffix('_per_tweet'))
+averages = averages.fillna(0)
+users = users.join(averages.add_suffix('_per_tweet'))
 
 ## Add number of unique places where the user has tweeted.  
 # This will be 0 if no places have ever been tagged
-genuine_users['unique_tweet_places'] = genuine_tweets.groupby('user_id').place.nunique()
-bot_users['unique_tweet_places'] = bot_tweets.groupby('user_id').place.nunique()
+users['unique_tweet_places'] = tweets.groupby('user_id').place.nunique()
 
 ## Update timestamp values to be datetime
-bot_tweets['timestamp_dt'] = pd.to_datetime(bot_tweets['timestamp'],format='%Y-%m-%d %H:%M:%S')
-genuine_tweets['timestamp_dt'] = pd.to_datetime(genuine_tweets['timestamp'],format='%Y-%m-%d %H:%M:%S')
-del bot_tweets['timestamp']
-del genuine_tweets['timestamp']
+tweets['timestamp_dt'] = pd.to_datetime(tweets['timestamp'],format='%Y-%m-%d %H:%M:%S')
+del tweets['timestamp']
 
 ## Add: Variance in the user's number of tweets per hour.
-genuine_tweets['date'] = genuine_tweets.timestamp_dt.dt.date
-genuine_tweets['hour'] = genuine_tweets.timestamp_dt.dt.hour
-variance_in_genuine_tweet_rate = genuine_tweets.groupby(['user_id','date','hour']).size().groupby('user_id').var()
-variance_in_genuine_tweet_rate.rename('variance_in_tweet_rate',inplace=True)
-genuine_users = genuine_users.join(variance_in_genuine_tweet_rate)
-bot_tweets['date'] = bot_tweets.timestamp_dt.dt.date
-bot_tweets['hour'] = bot_tweets.timestamp_dt.dt.hour
-variance_in_bot_tweet_rate = bot_tweets.groupby(['user_id','date','hour']).size().groupby('user_id').var()
+tweets['date'] = tweets.timestamp_dt.dt.date
+tweets['hour'] = tweets.timestamp_dt.dt.hour
+variance_in_bot_tweet_rate = tweets.groupby(['user_id','date','hour']).size().groupby('user_id').var()
 variance_in_bot_tweet_rate.rename('variance_in_tweet_rate',inplace=True)
-bot_users = bot_users.join(variance_in_bot_tweet_rate)
-
-## Delete empty values
-del bot_users['contributors_enabled'];
-del bot_users['follow_request_sent'];
-del bot_users['following'];
-del bot_users['notifications'];
-del bot_users['test_set_1'];
-del bot_users['test_set_2'];
-
-del genuine_users['contributors_enabled'];
-del genuine_users['follow_request_sent'];
-del genuine_users['following'];
-del genuine_users['notifications'];
-del genuine_users['test_set_1'];
-del genuine_users['test_set_2'];
-
-## Delete empty values
-del genuine_tweets['geo'];
-del genuine_tweets['contributors'];
-del genuine_tweets['favorited'];
-del genuine_tweets['retweeted'];
-
-del bot_tweets['geo'];
-del bot_tweets['contributors'];
-del bot_tweets['favorited'];
-del bot_tweets['retweeted'];
+users = users.join(variance_in_bot_tweet_rate)
 
 ## Feature selection code should go here (slimming down the users tables)
-
 ## Split data into train/dev/test 
 
-X = pd.concat([bot_users,genuine_users])
-y = pd.concat([pd.DataFrame(np.ones(shape=(len(bot_users),1))),pd.DataFrame(np.zeros(shape=(len(genuine_users),1)))])
-y.rename(columns={0:'is_bot'},inplace=True);
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+X_train, X_test, y_train, y_test = train_test_split(users, user_class, test_size=0.4)
 X_dev, X_test, y_dev, y_test = train_test_split(X_test, y_test, test_size=0.5)
