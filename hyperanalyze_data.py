70a87251e3bd1aa7c76c67ca1e611f5b6697d9d8
Sahil Nayyar <srjn94@gmail.com>
2017-12-08 13:35:03 -0800

    Added the neural network to analyze_data.py

--- hyperanalyze_data.py
+++ hyperanalyze_data.py
@@ -1,31 +1,71 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  7 09:15:23 2017
-
-@author: sahilnayyar
-"""
-
-import numpy as np
-import pickle
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV, ShuffleSplit
-from sklearn.metrics import fbeta_score
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
 Created on Wed Dec  6 16:49:08 2017
 
 @author: sahilnayyar
 """
+import os.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 from random import shuffle
-from sklearn import linear_model
-from sklearn.metrics import fbeta_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.neural_network import MLPClassifier
+
+###############################################################################
+
+def get_merged_data():
+    
+    X1 = np.asarray(pickle.load(open( "X_train.p", "rb" )));
+    X2 = np.asarray(pickle.load(open( "X_dev.p", "rb" )));
+    y1 = np.ravel(pickle.load(open( "y_train.p", "rb" )));
+    y2 = np.ravel(pickle.load(open( "y_dev.p", "rb" )));
+
+    X = np.concatenate((X1,X2));
+    y = np.concatenate((y1,y2));
+    groups = [-1 for i in range(len(X1))] + [1 for i in range(0,len(X2))]
+    
+    return X, y, groups;
+
+def getModelsOfPartitions(X_t, y_t, X_d, y_d, ft_list, fd_list, modelConstructor, args):
+    models = np.empty((len(ft_list), len(fd_list)), dtype=object);
+    for i in range(0,len(ft_list)):
+        for j in range(0,len(fd_list)):
+            
+            ft = ft_list[i];
+            fd = fd_list[j];
+        
+            X_train, y_train, X_dev, y_dev = \
+                repartitionExamples(X_t, y_t, X_d, y_d, ft, fd)
+                        
+            model = modelConstructor(**args);
+            model.fit(np.asmatrix(X_train), np.ravel(y_train));
+            models[i,j] = model;
+            
+    return models;
+
+def mapFromModelGrid(f, grid):
+    (m, n) = np.shape(grid);
+    out = np.empty((m, n), dtype = float);
+    for i in range(0,m):
+        for j in range(0,m):
+            out[i,j] = f(grid[i,j]);
+    return out;
+
+def hyper_fit(X, y, model_constructor, cv, fixed, var, filename = ""):
+    
+    model = model_constructor(**fixed);
+    hyper = GridSearchCV(model, var, cv = cv, return_train_score=True, scoring = make_scorer(scorerfun));
+    hyper.fit(X, y);
+    
+    if filename != "":
+        pickle.dump(hyper.best_estimator_, open("filename","wb"));
+        
+    return hyper;
 
 def repartitionExamples(X_t, y_t, X_d, y_d, perc_t, perc_d):
     m_t = len(X_t);
@@ -50,92 +90,70 @@ def repartitionExamples(X_t, y_t, X_d, y_d, perc_t, perc_d):
     
     return X_t_new, y_t_new, X_d_new, y_d_new;
 
-def getModelsOfPartitions(X_t, y_t, X_d, y_d, ft_list, fd_list, modelConstructor, args):
-    models = np.empty(np.shape(ft_grid), dtype=object);
-    for i in range(0,len(ft_list)):
-        for j in range(0,len(fd_list)):
-            
-            ft = ft_list[i];
-            fd = fd_list[j];
-        
-            X_train, y_train, X_dev, y_dev = \
-                repartitionExamples(X_t, y_t, X_d, y_d, ft, fd)
-                        
-            model = modelConstructor(**args);
-            model.fit(np.asmatrix(X_train), np.ravel(y_train));
-            models[i,j] = model;
-            
-    return models;
+def scorerfun(y_true, y_pred):
+    return fbeta_score(y_true,y_pred,0.5);
 
-def mapFromModelGrid(f, grid):
-    (m, n) = np.shape(grid);
-    out = np.empty((m, n), dtype = float);
-    for i in range(0,m):
-        for j in range(0,m):
-            out[i,j] = f(grid[i,j]);
-    return out;
+###############################################################################
 
-X_train = pickle.load(open( "X_train.p", "rb" ));
-y_train = pickle.load(open( "y_train.p", "rb" ));
-X_dev = pickle.load(open( "X_dev.p", "rb" ));
-y_dev = pickle.load(open( "y_dev.p", "rb" ));
+# Neural network with more data
 
-ft_list = np.arange(.1,1,.1);
-fd_list = np.arange(0,.9,.1);
-ft_grid, fd_grid = np.meshgrid(ft_list, fd_list)
-models = getModelsOfPartitions(X_train, y_train, X_dev, y_dev, ft_list, fd_list, linear_model.LogisticRegression, dict(C=0.79));
+X, y, test_fold = get_merged_data();
 
-BETA = 0.5;
-fscoreFun = lambda m: fbeta_score(m.predict(X_dev), np.ravel(y_dev), BETA);
-fscores = mapFromModelGrid(fscoreFun, models);
+max_layers = 20;
+layersize_tuples = [(i,j) for i in range(1,max_layers+1) for j in range(1,max_layers+1)];
 
-plt.axes().set_aspect('equal');
-plt.pcolor(ft_grid, fd_grid, fscores);
-plt.title('F-Score vs. Partitions of Datasets');
-plt.xlabel('Fraction of Original Training Set');
-plt.ylabel('Fraction of Original Dev Set');
-plt.colorbar();
-
-X_dev2 = pickle.load(open( "X_test.p", "rb" ));
-y_dev2 = pickle.load(open( "y_test.p", "rb" ));
-
-def hyper_fit(X, y, model_constructor, cv, fixed, var, filename = ""):
+if not os.path.isfile('nn_model.p'):
+    model = hyper_fit( \
+            X, y, 
+            MLPClassifier, 
+            PredefinedSplit(test_fold),
+            {"solver":'lbfgs', 'alpha':0.01, 'random_state':1}, 
+            {'hidden_layer_sizes':[(i,j) for i in range(1,max_layers+1) for j in range(1,max_layers+1)]},
+            "nn_model.p"
+    );
     
-    model = model_constructor(**fixed);
-    hyper = GridSearchCV(model, var, cv = cv, return_train_score=True);
-    hyper.fit(X, y);
+else:
+    model = pickle.load(open("nn_model.p", "rb"));
     
-    if filename != "":
-        pickle.dump(hyper, open("filename","wb"));
-        
-    return hyper;
 
-def get_merged_data():
-    
-    X1 = np.asarray(pickle.load(open( "X_train.p", "rb" )));
-    X2 = np.asarray(pickle.load(open( "X_dev.p", "rb" )));
-    y1 = np.ravel(pickle.load(open( "y_train.p", "rb" )));
-    y2 = np.ravel(pickle.load(open( "y_dev.p", "rb" )));
+#layer1size_list = layer2size_list = range(1,max_layers+1);
+#layer1size_grid, layer2size_grid = np.meshgrid(layer1size_list, 
+#                                               layer2size_list);
+#fscores = np.reshape(clf.cv_results_['split0_test_score'], 
+#                           (max_layers,max_layers));
+#                           
+#plt.axes().set_aspect('equal');
+#plt.pcolor(layer1size_grid, layer2size_grid, fscores);
+#plt.title('Neural Net F-Score vs. Layer Sizes');
+#plt.xlabel('Layer 1');
+#plt.ylabel('Layer 2');
+#plt.colorbar();
 
-    X = np.concatenate((X1,X2));
-    y = np.concatenate((y1,y2));
-    groups = [1 for i in range(len(X1))] + [2 for i in range(0,len(X2))]
-    
-    return X, y, groups;
+ 
+## Partitioning data (old)
 
-#(X, y), splitloc = get_merged_data();
-#newsplitloc = int((splitloc + (len(X)-splitloc)/3));
-#X_train = X[0: newsplitloc]
-#y_train = y[0: newsplitloc]
-#X_dev = X[newsplitloc+1: len(X)]
-#y_dev = y[newsplitloc+1: len(X)]
+#X_train = pickle.load(open( "X_train.p", "rb" ));
+#y_train = pickle.load(open( "y_train.p", "rb" ));
+#X_dev = pickle.load(open( "X_dev.p", "rb" ));
+#y_dev = pickle.load(open( "y_dev.p", "rb" ));
 #
-#clf = hyper_fit( \
-#        X, y, 
-#        LogisticRegression, 
-#        ShuffleSplit(), 
-#        {"penalty":"l2"}, 
-#        {"C":np.logspace(-2,2,5)}
-#);
+#ft_list = np.arange(.1,1,.1);
+#fd_list = np.arange(0,.9,.1);
+#ft_grid, fd_grid = np.meshgrid(ft_list, fd_list)
+#models = getModelsOfPartitions(X_train, y_train, X_dev, y_dev, ft_list, fd_list, linear_model.LogisticRegression, dict(C=0.79));
 #
-#print(fbeta_score(clf.predict(X_dev), y_dev, 0.5));
+#BETA = 0.5;
+#fscoreFun = lambda m: fbeta_score(m.predict(X_dev), np.ravel(y_dev), BETA);
+#fscores = mapFromModelGrid(fscoreFun, models);
+#
+#plt.axes().set_aspect('equal');
+#plt.pcolor(ft_grid, fd_grid, fscores);
+#plt.title('F-Score vs. Partitions of Datasets');
+#plt.xlabel('Fraction of Original Training Set');
+#plt.ylabel('Fraction of Original Dev Set');
+#plt.colorbar();
+#
+#X_dev2 = pickle.load(open( "X_test.p", "rb" ));
+#y_dev2 = pickle.load(open( "y_test.p", "rb" ));
+    
+
